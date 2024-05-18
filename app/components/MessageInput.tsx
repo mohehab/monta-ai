beee745a9ae2281bd2ff8a2dd8be4ee4a4c939eb
Mohamed Ehab <63422773+mohehab@users.noreply.github.com>
2024-05-18 15:51:10 +0300

    Add files via upload

--- app/components/MessageInput.tsx
+++ app/components/MessageInput.tsx
@@ -8,8 +8,8 @@ export default function MessageInput() {
     const [text, setText] = useState('')
     const messagesListContext = useContext(MessagesListContext)
 
-    const handleSendMessage = (e: KeyboardEvent) => {
-      if (text && e.key === 'Enter' || text && e.type === 'click') {
+    const handleSendMessage = (key: string, type: string) => {
+      if (text && key === 'Enter' || text && type === 'click') {
         messagesListContext.setUserMessages([...messagesListContext.userMessages, text])
         localStorage.setItem('Messages', JSON.stringify([...messagesListContext.userMessages, text]))
         setText('')
@@ -19,9 +19,9 @@ export default function MessageInput() {
     return (
       <div className="bottom-0 relative my-2.5 w-full px-5 sm:px-24 lg:px-52">
         <div className="relative">
-          <textarea className="w-full text-sm rounded-xl p-3.5 border-indigo-monta border" name="Message" placeholder="Message" rows={1} value={text} onChange={(e) => setText(e.target.value)} onKeyUp={(e) => handleSendMessage(e)}></textarea>
+          <textarea className="w-full text-sm rounded-xl p-3.5 border-indigo-monta border" name="Message" placeholder="Message" rows={1} value={text} onChange={(e) => setText(e.target.value)} onKeyUp={(e) => handleSendMessage(e.key, '')}></textarea>
           <div className="absolute cursor-pointer text-xl top-3.5 right-8">
-            <FontAwesomeIcon title="Send" icon={faPaperPlane} onClick={(e) => handleSendMessage(e)} />
+            <FontAwesomeIcon title="Send" icon={faPaperPlane} onClick={(e) => handleSendMessage('', e.type)} />
           </div>
         </div>
       </div>
